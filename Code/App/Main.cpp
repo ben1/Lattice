b8e3aab63433adb374733404bf509322e5d70d27
Benjamin Wooller <ben135@gmail.com>
2023-01-07 18:59:55 +1000

    Move temporary files generated by the runtim into tmp.

--- Code/App/Main.cpp
+++ Code/App/Main.cpp
@@ -51,18 +51,17 @@ int main(int argc, char*argv[])
     SDL_GL_MakeCurrent(window, gl_context);
     SDL_GL_SetSwapInterval(1); // Enable vsync
 
-    // Setup Dear ImGui context
+    // Setup Dear ImGui
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
-    //io.ConfigViewportsNoAutoMerge = true;
     io.ConfigViewportsNoTaskBarIcon = true;
+    io.IniFilename = "tmp/imgui.ini";
+    io.LogFilename = "tmp/imgui_log.txt";
 
-    // Setup Dear ImGui style
     ImGui::StyleColorsDark();
 
     // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
@@ -77,11 +76,11 @@ int main(int argc, char*argv[])
     ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    AppInit();
-
     // Load Default Font
     io.Fonts->AddFontFromFileTTF("Data/Fonts/Cousine-Regular.ttf", 18.0f);
     const ImVec4 kClearColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    AppInit();
 
     // Main loop
     bool done = false;
