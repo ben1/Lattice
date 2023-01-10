9e216d27d6bed4ed6e9647e1e0fe2b9bf28d9726
Benjamin Wooller <ben135@gmail.com>
2023-01-10 22:54:53 +1000

    some sim additions

--- Code/Sim/World.h
+++ Code/Sim/World.h
@@ -3,7 +3,42 @@
 #include <Core/Containers/Array.h>
 #include <Core/Strings/AString.h>
 
+
+class BuildingType
+{
+
+};
+
+class Building
+{
+
+};
+
+class Settlement
+{
+    uint64_t mId;
+    AString mName;
+
+
+    Array<Building> mBuildings;
+};
+
+class Land
+{
+    float mForested = 0;
+    float mSoil = 0;
+    float mGold = 0;
+    float mIron = 0;
+    float mFarmed = 0;
+};
+
+
+
+
 class World
 {
 
+
+
+    Array<Settlement> mSettlements;
 };
